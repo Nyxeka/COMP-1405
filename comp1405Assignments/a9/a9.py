96153d71597fca23bdc4570d5c6bee605ec61a74
Nyxeka <me@nickhylands.com>
2013-11-24 23:36:24 -0500

    done a9, q8 finished.

--- comp1405Assignments/a9/a9.py
+++ comp1405Assignments/a9/a9.py
@@ -18,23 +18,25 @@ def fib3(n):
 #Question 3:
 def largest_two(a):
     b = a
-    largest = b.max()
+    largest = max(b)
     b.remove(largest)
-    second_largest = b.max()
+    second_largest = max(b)
     return largest + second_largest
 
 #Question 4:
 def smallest_half(a):
     number_of_values = len(a)/2
-    b = sort(a)
-    c = b[(len(a)/2):]
-    return sum(c)
+    a.sort()
+    lowest_values = a[int(len(a)/2):]
+    return sum(lowest_values)
 
 #Question 5:
 def median(a):
-    b = sort(a)
-    half = len(a)/2
+    b = a
+    b.sort()
+    half = int(len(a)/2)
     x = (b[half] + b[half+1])/2
+    return x
 
 #Question 6:
 def majority(a):
@@ -47,8 +49,6 @@ def majority(a):
             b[i] = 1
     values = list(b.values())
     keys = list(b.keys())
-    #the following was found here as the fastest method of grabbing the key of the biggest value in a fict
-    #http://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
     biggest = keys[values.index(max(values))]
     if b[biggest] > (len(a)/2 + 1):
         return biggest
@@ -57,78 +57,43 @@ def majority(a):
 
 #Question 7:
 
-def make_change(n,b=dict()):
-    c = b
+def make_change(n,amounts):
+    c = dict()
     
-    if len(c) < 1:
-        c[100] = 0
-        c[50] = 0
-        c[20] = 0
-        c[10] = 0
-        c[5] = 0
-        c[2] = 0
-        c[1] = 0
-        c[0.25] = 0
-        c[0.1] = 0
-        c[0.05] = 0
-        x = float(n)/10
-    else:
-        x = n
-    if x > 0:
-        if n >= 100:
-            c[100] += 1
-            x -= 100
-        if x >= 50:
-            c[50] += 1
-            x -= 50
-        if x >= 20:
-            c[20] += 1
-            x -= 20
-        if x >= 10:
-            c[10] += 1
-            x -= 10
-        if x >= 5:
-            c[5] += 1
-            x -= 5
-        if x >= 2:
-            c[2] += 1
-            x -= 2
-        if x >= 1:
-            c[1] += 1
-            x -= 1
-        if x >= 0.25:
-            c[0.25] += 1
-            x -= 0.25
-        if x >= 0.1:
-            c[0.1] += 1
-            x -= 0.1
-        if x >= 0.05:
-            c[0.05] += 1
-            x -= 0.05
-        return make_change(x,c)
-    else:
-        return c
+    for i in amounts:
+        c[i] = 0
+    
+    x = float(n)/100
+    
+    for i in amounts:
+        to_keep = x % i
+        to_remove = x - to_keep
+        
+        c[i] += int(to_remove / i)
+        x = to_keep
+    return c
     
 
 def canadian_change(n):
     rounded_cents = int(5 * round(float(n)/5))
     print "$%.2f is rounded to $%.2f" % (float(n)/100, float(rounded_cents)/100)
     amounts = [100,50,20,10,5,2,1,0.25,0.1,0.05]
-    change = make_change(rounded_cents)
+    change = make_change(rounded_cents,amounts)
     if len(change) > 0:
         for i in amounts:
-            if c[i] > 0:
-                print str(c[i]) + " x $" + str(i)
+            if change[i] > 0:
+                print str(change[i]) + " x $" + str(i)
                 
 #Question 8:
 def triple_sum(a, x):
     result = []
     b = set(a)
+    num_times = 0
     done = False
     for i in range(len(a)-1):
         for j in range(i+1, len(a)):
             number = x-(a[i]+a[j])
-            
+            num_times += 1
             if number in b:
                 result.append(i)
                 
@@ -140,6 +105,6 @@ def triple_sum(a, x):
                 break
         if done:
             break
+    if result == []:
+        result = None
     return result
-    
-    
